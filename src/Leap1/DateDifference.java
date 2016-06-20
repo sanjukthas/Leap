5fb0530696152d39ef164332568cd8d21e5ccfc0
sanjukthas <sanjuktha23@gmail.com>
2016-06-20 15:43:31 -0400

    leap

---
+++ src/Leap1/DateDifference.java
@@ -0,0 +1,25 @@
+package Leap1;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class DateDifference {
+
+	private String daysBetween(Date time, Date time2) {
+	DateDifference difference = new DateDifference();
+			}
+			DateDifference() {
+			Calendar cal1 = new GregorianCalendar();
+			Calendar cal2 = new GregorianCalendar();
+
+			cal1.set(2010, 12, 1); 
+			cal2.set(2011, 9, 31);
+			System.out.println("Days= "+daysBetween(cal1.getTime(),cal2.getTime()));
+			
+				return null;
+			}
+			
+			
+	}
+
